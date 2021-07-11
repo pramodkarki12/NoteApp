db1f76faf2f3cae4fdc06fb29787aa665a7621db
pramodkarki12 <np03a190229@heraldcollege.edu.np>
2021-07-11 09:32:20 +0545

    Creating an Entity Class

--- app/src/main/java/com/pramodkarki/noteapp/Notes.java
+++ app/src/main/java/com/pramodkarki/noteapp/Notes.java
@@ -1,7 +1,29 @@
 package com.pramodkarki.noteapp;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
-@Entity(tableName = "NoteDB")
+@Entity(tableName = "NotesDB")
 public class Notes {
+
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    @ColumnInfo(name = "notes_title")
+    String notesTitle;
+
+    @ColumnInfo(name = "notes_subtitle")
+    String notesSubTitle;
+
+    @ColumnInfo(name = "notes")
+    String notes;
+
+    @ColumnInfo(name = "notes_date")
+    String notesDate;
+
+    @ColumnInfo(name = "notes_priority")
+    String notesPriority;
+
+
 }
